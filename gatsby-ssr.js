a277a322eae2f0d7c9f603d9cf155eef7d674785
Ahmed <a.aldwaeb@gmail.com>
2021-05-03 10:01:52 +0300

    fix: ssr bug

---
+++ gatsby-ssr.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { DarkTheme, DefaultTheme } from './src/Theme'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { ThemeContextProvider, useThemeContext } from './src/context/ThemeContext'
+import '@fontsource/open-sans'
+import { Helmet } from 'react-helmet'
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: "Open Sans";
+  }
+
+  body {
+    transition: background-color .3s;
+    background-color: ${({ theme }) => theme.background};
+  }
+`
+
+export const wrapRootElement = ({ element }) => {
+  return (
+    <ThemeContextProvider>
+      <Helmet title="Landing Site" htmlAttributes={{ lang: 'en' }} />
+      <SCThemeProvider>
+        <GlobalStyles />
+        {element}
+      </SCThemeProvider>
+    </ThemeContextProvider>
+  )
+}
+
+const SCThemeProvider = ({ children }) => {
+  const { currentTheme } = useThemeContext()
+
+  return (
+    <ThemeProvider theme={currentTheme === 'dark' ? DarkTheme : DefaultTheme}>
+      {children}
+    </ThemeProvider>
+  )
+}
